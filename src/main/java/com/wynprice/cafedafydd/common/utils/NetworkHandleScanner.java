2b9045dc1ed61c6520e60627135d1625dac7696b
Wyn-Price <wynprice999@gmail.com>
2019-09-09 22:30:00 +0100

    Added better logging to network handle scanner

--- src/main/java/com/wynprice/cafedafydd/common/utils/NetworkHandleScanner.java
+++ src/main/java/com/wynprice/cafedafydd/common/utils/NetworkHandleScanner.java
@@ -4,26 +4,37 @@ import com.wynprice.cafedafydd.common.netty.NetworkHandler;
 import lombok.extern.log4j.Log4j2;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Log4j2
 public class NetworkHandleScanner {
-    public static NetworkConsumer generate(Class baseClass) {
+    private static final Matcher LAMBDA_METHOD_PATTERN = Pattern.compile("lambda\\$(.+)\\$\\d+").matcher("");
+
+    public static NetworkConsumer generateConsumer(Class baseClass) {
         Map<Class, BiConsumer<NetworkHandler, Object>> maps = new HashMap<>();
         log.info("Scanning class " + baseClass + " for network handles");
         for (Method method : baseClass.getDeclaredMethods()) {
-            log.info("Scanning method " + method.getName() + " for network handles");
-            if(method.getAnnotation(NetworkHandle.class) != null && method.getParameterCount() == 1) {
-                log.info("Added method '" + method.getName() + "' to accept '" + method.getParameterTypes()[0] + "'");
-                maps.put(method.getParameterTypes()[0], (network, packet) -> {
-                    try {
-                        method.invoke(network, packet);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+            if(method.getAnnotation(NetworkHandle.class) != null) {
+                StringBuilder builder = new StringBuilder(String.format("%-45s", "Scanning method " + method.getName()) + ": ");
+                if(method.getParameterCount() == 1) {
+                    builder.append(method.getParameterTypes()[0].getSimpleName());
+                    maps.put(method.getParameterTypes()[0], (network, packet) -> {
+                        try {
+                            method.invoke(network, packet);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                } else {
+                    builder.append("Too many arguments!");
+                }
+                log.info(builder);
             }
         }
         return (handle, packet) ->
