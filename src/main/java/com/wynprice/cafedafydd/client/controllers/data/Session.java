1fcc8c58ccbb5494c717ed559e267678230036b6
Wyn Price <wynprice999@gmail.com>
2019-10-15 10:30:16 +0100

    Documentation for Session.java

--- src/main/java/com/wynprice/cafedafydd/client/controllers/data/Session.java
+++ src/main/java/com/wynprice/cafedafydd/client/controllers/data/Session.java
@@ -9,13 +9,42 @@ import java.util.Date;
 
 import static com.wynprice.cafedafydd.common.DatabaseStrings.Sessions;
 
+/**
+ * Sessions holds data about a current session. Used by {@link com.wynprice.cafedafydd.client.controllers.SearchSessionsPage} 
+ * and {@link com.wynprice.cafedafydd.client.controllers.UserLoginPage}. <br>
+ * This is mainly used to delegate the {@link #toString} method, to allow for a customized visulization on the listview. 
+ * It also caches the fields from the record. 
+ */
 @Data
 public class Session {
+    /**
+     * The primary field id 
+     */
     private final int fieldID;
+    
+    /**
+     * The id of the computer used in this session 
+     */
     private final int computerID;
+    
+    /**
+     * The datetime of when this session was started. 
+     */
     private final Date startDate;
+    
+    /**
+     * The datetime of when this session ended, or Unix epoch if the session has not ended.
+     */
     private final Date endDate;
+    
+    /**
+     * The calculated price for this database, or 0 if the session has not ended.
+     */
     private final float price;
+    
+    /**
+     * The datetime of when this session ended, or Unix epoch if the session has not ended.
+     */
     private final boolean hasPaid;
 
     public static Session fromRecord(DatabaseRecord record) {
